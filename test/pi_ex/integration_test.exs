db2ad5e542fda337a29057f908bc5fbcacfe6fbd
Chay Choong <hi@chay.dev>
2026-02-27 14:26:36 +0800

    test: add integration tests with fake Pi script

---
+++ test/pi_ex/integration_test.exs
@@ -0,0 +1,55 @@
+defmodule PiEx.IntegrationTest do
+  use ExUnit.Case
+
+  @moduletag :integration
+
+  @fake_pi Path.expand("../support/fake_pi.sh", __DIR__)
+
+  test "full prompt lifecycle" do
+    {:ok, pid} = PiEx.Instance.start_link(pi_path: @fake_pi)
+
+    PiEx.prompt(pid, "hello")
+
+    assert_receive {:pi_event, _, %PiEx.Event.AgentStart{}}, 5000
+
+    assert_receive {:pi_event, _, %PiEx.Event.MessageUpdate{type: :text_delta, text: "Hello from fake pi"}},
+                   5000
+
+    assert_receive {:pi_event, _, %PiEx.Event.AgentEnd{}}, 5000
+
+    GenServer.stop(pid, :normal)
+  end
+
+  test "get_state returns response" do
+    {:ok, pid} = PiEx.Instance.start_link(pi_path: @fake_pi)
+
+    assert {:ok, %PiEx.Response{success: true, data: data}} = PiEx.get_state(pid)
+    assert data["isStreaming"] == false
+
+    GenServer.stop(pid, :normal)
+  end
+
+  test "delta accumulation over full stream" do
+    {:ok, pid} = PiEx.Instance.start_link(pi_path: @fake_pi)
+    PiEx.prompt(pid, "hello")
+
+    delta = collect_delta(PiEx.Delta.new())
+    assert PiEx.Delta.text(delta) == "Hello from fake pi"
+    assert PiEx.Delta.done?(delta)
+
+    GenServer.stop(pid, :normal)
+  end
+
+  defp collect_delta(delta) do
+    receive do
+      {:pi_event, _, %PiEx.Event.MessageUpdate{} = event} ->
+        delta = PiEx.Delta.apply_event(delta, event)
+        if event.type == :done, do: delta, else: collect_delta(delta)
+
+      {:pi_event, _, _} ->
+        collect_delta(delta)
+    after
+      5000 -> delta
+    end
+  end
+end
---
+++ test/support/fake_pi.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Fake Pi RPC server for integration testing.
+# Reads JSON commands from stdin, writes JSON events/responses to stdout.
+
+while IFS= read -r line; do
+  command=$(echo "$line" | jq -r '.command // ""')
+  id=$(echo "$line" | jq -r '.id // ""')
+
+  case "$command" in
+    prompt)
+      echo '{"type":"agent_start"}'
+      echo '{"type":"turn_start"}'
+      echo '{"type":"message_start"}'
+      echo '{"type":"message_update","assistantMessageEvent":{"type":"text_start"}}'
+      echo '{"type":"message_update","assistantMessageEvent":{"type":"text_delta","text":"Hello from fake pi"}}'
+      echo '{"type":"message_update","assistantMessageEvent":{"type":"text_end"}}'
+      echo '{"type":"message_update","assistantMessageEvent":{"type":"done","reason":"stop"}}'
+      echo '{"type":"message_end"}'
+      echo '{"type":"turn_end"}'
+      echo '{"type":"agent_end","messages":[]}'
+      ;;
+    get_state)
+      echo "{\"type\":\"response\",\"command\":\"get_state\",\"success\":true,\"id\":\"$id\",\"data\":{\"isStreaming\":false}}"
+      ;;
+    *)
+      echo "{\"type\":\"response\",\"command\":\"$command\",\"success\":true,\"id\":\"$id\",\"data\":{}}"
+      ;;
+  esac
+done
